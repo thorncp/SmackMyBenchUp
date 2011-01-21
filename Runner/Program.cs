f5fac78cb151c8f794d0af1d019f5293aa31dba2
Chris Thorn <thorncp@gmail.com>
2011-01-20 17:19:16 -0900

    trying to get a feel for a decent api

---
+++ Runner/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using SmackMyBenchUp;
+
+namespace Runner
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Benchmark.Profile(10, new Dictionary<string, Action> {
+                { "hi", () => { int x = 42; } },
+                { "yo", () => { int x = 1337; } }
+            });
+
+            Benchmark.Profile2(10, bench => {
+                bench.Blar("hi", () => { int x = 42; });
+                bench.Blar("yo", () => { int x = 1337; });
+            });
+        }
+    }
+}
---
+++ SmackMyBenchUp/Benchmark.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace SmackMyBenchUp
+{
+    public static class Benchmark
+    {
+        public static void Profile(int runs, Dictionary<string, Action> actions)
+        {
+            foreach (var action in actions)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+
+                var times = new List<long>();
+
+                for (int i = 0; i < runs; i++)
+                {
+                    stopwatch.Start();
+                    action.Value();
+                    stopwatch.Stop();
+                    times.Add(stopwatch.ElapsedMilliseconds);
+                    stopwatch.Reset();
+                }
+
+                Console.Out.WriteLine(string.Format("{0} averaged {1}ms", action.Key, times.Average()));
+            }
+        }
+
+        public static void Profile2(int runs, Action<Bench> action)
+        {
+            Bench bench = new Bench();
+            action(bench);
+
+            foreach (var b in bench)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+
+                var times = new List<long>();
+
+                for (int i = 0; i < runs; i++)
+                {
+                    stopwatch.Start();
+                    b.Value();
+                    stopwatch.Stop();
+                    times.Add(stopwatch.ElapsedMilliseconds);
+                    stopwatch.Reset();
+                }
+
+                Console.Out.WriteLine(string.Format("{0} averaged {1}ms", b.Key, times.Average()));
+            }
+        }
+    }
+
+    // todo: better name
+    public class Bench : Dictionary<string, Action>
+    {
+        public void Blar(string label, Action action)
+        {
+            Add(label, action);
+        }
+    }
+}
